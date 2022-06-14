b84d0a5d99e6145dc5f7005f85baab1bbaaa73b2
bt66 <bastian.bt66@gmail.com>
2022-06-15 05:54:38 +0700

    first

---
+++ esp32/getDataset/getDataset.ino
@@ -0,0 +1,55 @@
+#include "WiFi.h"
+#include <HTTPClient.h>
+
+const char* ssid = "bt66_1";
+const char* password = "2wsx1qaz";
+const int temperaturePin = 32;
+const int humidityPin = 33;
+
+
+String datasetAPI ="https://fp-ai.tukutahu.tk/insertData";
+
+
+HTTPClient http;
+
+
+void setup() {
+  Serial.begin(115200);
+ 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+ 
+  Serial.println("Connected to the WiFi network");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+    if(WiFi.status() == WL_CONNECTED){
+
+        http.begin(datasetAPI.c_str());
+        http.addHeader("Content-Type", "application/json");
+        String temperature = String(analogRead(temperaturePin));
+        String humidity = String(analogRead(humidityPin));
+        String reqBody = "{\"deviceId\":\"esp32-3\",\"tempereture\":"+ temperature +",\"humidity\":"+ humidity +"}";
+        Serial.println("-----------------------------");
+        Serial.println(reqBody);
+        int httpResponseCode = http.POST(reqBody);
+
+        if (httpResponseCode == 200) {
+            Serial.print("HTTP response code: ");
+            Serial.println(httpResponseCode);
+        }
+        else {
+            Serial.print("Error post data code: ");
+            Serial.println(httpResponseCode);
+            Serial.println(http.getString());
+        }
+        http.end();
+        delay(3000);
+
+    }
+}
